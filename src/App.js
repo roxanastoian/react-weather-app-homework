d68ae0217bc8417dc8863f489235d99e2fdfa20d
roxanastoian <131897578+roxanastoian@users.noreply.github.com>
2024-02-02 22:17:25 +0000

    Added HTML structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather App
+        <Weather />
         <footer>
           <p>This project was coded by Roxana Stoian and is</p>
           <a
