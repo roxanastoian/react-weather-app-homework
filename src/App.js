9d99a0c014a10d7a4845441652bf7386c4d57dcf
roxanastoian <131897578+roxanastoian@users.noreply.github.com>
2024-02-02 21:49:28 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        Weather App
+        <footer>
+          <p>This project was coded by Roxana Stoian and is</p>
+          <a
+            href="https://github.com/roxanastoian/react-weather-app-homework"
+            target="_blank"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
